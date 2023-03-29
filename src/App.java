bca38eabcbcc7fb44089f73e4b20fd8d6bc6977e
yagowill <yagowill3@gmail.com>
2023-03-28 23:40:38 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,33 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+import java.lang.Integer;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // fazer uma conexão HTTP e buscar os tops 250 filmes
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        URI endereco = URI.create(url);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(endereco).build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+
+        // extrair só os dados que interessam (titulo, poster, classificação)
+        JsonParser parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body); 
+        // exibir e manipular os dados
+        for (Map<String,String> filme : listaDeFilmes) {
+            System.out.println("\u001b[37m\u001b[45mAvaliação: " + filme.get("imDbRating") + "\u001b[m");
+            System.out.print("\u2B50");
+            System.out.println("Title: " + "\u001b[1m" + filme.get("title") + "\u001b[m");
+            System.out.println("Poster: " +"\u001b[1m" + filme.get("image") + "\u001b[m");
+            System.out.println();
+        }
+    }
+}
