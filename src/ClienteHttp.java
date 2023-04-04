5f11a29805575837a62092219a3582cdc97d940f
yagowill <yagowill3@gmail.com>
2023-04-04 20:19:47 -0300

    custom erros

--- src/ClienteHttp.java
+++ src/ClienteHttp.java
@@ -17,7 +17,7 @@ public class ClienteHttp {
             return body;
             
         } catch (IOException | InterruptedException e) {
-            throw new RuntimeException(e);
+            throw new ClienteHttpException("Erro ao consultar a URL. :(");
         }
     }
 }
